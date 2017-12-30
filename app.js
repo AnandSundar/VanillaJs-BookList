eca8ec5b9734e68463eacf0e7a161bdbac98c054
AnandSundar <anandsundar96@gmail.com>
2017-12-30 00:31:15 -0500

    Delete boom from list

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 }
 
+//delete book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 //clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -80,4 +87,17 @@ document.getElementById('book-form').addEventListener('submit', function (e){
   }
 
   e.preventDefault();
+});
+
+//event listener for delete
+document.getElementById('book-list').addEventListener('click', function (e){
+  
+ //Instantiate UI
+ const ui = new UI();
+ //delete book
+ ui.deleteBook(e.target);
+
+ //show alert msg
+ ui.showAlert('Book successfully Deleted', 'success');
+
 });
