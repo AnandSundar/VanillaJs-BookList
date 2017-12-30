db40796566c84ed5b7e9d570002da5641346ba6f
AnandSundar <anandsundar96@gmail.com>
2017-12-30 00:12:45 -0500

    implemented validation and alert

--- app.js
+++ app.js
@@ -25,6 +25,27 @@ UI.prototype.addBookToList = function(book) {
   list.appendChild(row);
 }
 
+//show alert
+UI.prototype.showAlert = function (message, className) {
+  //create div
+  const div = document.createElement('div');
+  //add classes
+  div.className = `alert ${className}`;
+  //add text
+  div.appendChild(document.createTextNode(message));
+  //get parent
+  const container = document.querySelector('.container');
+  //get form
+  const form = document.getElementById('book-form');
+  //insert alert
+  container.insertBefore(div, form);
+
+  //timeout after 3sec
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 //clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -45,10 +66,18 @@ document.getElementById('book-form').addEventListener('submit', function (e){
   //Instantiate UI
   const ui = new UI();
 
-  //add book to list
-  ui.addBookToList(book);
-  //clear fields
-  ui.clearFields();
+  //validate
+  if(title === '' || author === '' || isbn === '') {
+    //error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  }else {
+    //add book to list
+    ui.addBookToList(book);
+    //clear fields
+    ui.clearFields();
+    //show success msg
+    ui.showAlert('Book successfully added', 'success');
+  }
 
   e.preventDefault();
 });
